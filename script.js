a2744476df8850570cd35b208d14c711c6879b22
catagonus <107582977+catagonus@users.noreply.github.com>
2023-12-22 14:26:47 -0500

    Add playRound function to play 1 round of rock paper scissors

--- script.js
+++ script.js
@@ -22,3 +22,42 @@ function getPlayerChoice() {
         playerChoice.slice(1).toLocaleLowerCase();
     return playerChoice;
 }
+
+function playRound(playerChoice, computerChoice) {
+    console.log(`Player choice: ${playerChoice}`);
+    console.log(`Computer choice: ${computerChoice}`);
+
+    if (playerChoice === computerChoice) {
+        console.log("Result: tie");
+        alert("You tied! Try again.");
+        return playRound(getPlayerChoice(), getComputerChoice());
+    } else if (playerChoice === "Rock") {
+        if (computerChoice === "Scissors") {
+            console.log("Result: player victory");
+            return "You win! Rock beats scissors";
+        } else {
+            console.log("Result: computer victory");
+            return "You lose! Paper beats rock"
+        }
+    } else if (playerChoice === "Paper") {
+        if (computerChoice === "Rock") {
+            console.log("Result: player victory");
+            return "You win! Paper beats rock.";
+        } else {
+            console.log("Result: computer victory");
+            return "You lose! Scissors beat paper."
+        }
+    } else if (playerChoice === "Scissors") {
+        if (computerChoice === "Paper") {
+            console.log("Result: player victory");
+            return "You win! Scissors beat paper.";
+        } else {
+            console.log("Result: computer victory");
+            return "You lose! Rock beats scissors."
+        }
+    } else {
+        console.log("Result: player entered invalid choice");
+        alert("Please play by the rules! Enter only Rock, Paper, or Scissors.");
+        return playRound(getPlayerChoice(), getComputerChoice());
+    }
+}
